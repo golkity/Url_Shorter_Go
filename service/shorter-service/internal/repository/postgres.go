3eca2a6d8e2361ef720472651ed71ca64f37f64b
Finnik <73883383+golkity@users.noreply.github.com>
2025-09-25 10:34:55 +0300

    feat(repo): add new method and add resiled this method

--- service/shorter-service/internal/repository/postgres.go
+++ service/shorter-service/internal/repository/postgres.go
@@ -3,9 +3,10 @@ package repository
 import (
 	"context"
 	"errors"
+	"url-shortener/internal/custom_errors"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"url-shortener/internal/custom_errors"
 )
 
 type PostgresRepository struct {
@@ -16,9 +17,9 @@ func NewPostgresRepository(db *pgxpool.Pool) *PostgresRepository {
 	return &PostgresRepository{db: db}
 }
 
-func (r *PostgresRepository) SaveURL(ctx context.Context, fullURL, shortURL string) error {
-	query := `INSERT INTO urls (full_url, short_url) VALUES ($1, $2)`
-	_, err := r.db.Exec(ctx, query, fullURL, shortURL)
+func (r *PostgresRepository) SaveURL(ctx context.Context, fullURL, shortURL, userID string) error {
+	query := `INSERT INTO urls (full_url, short_url, user_id) VALUES ($1, $2, $3)`
+	_, err := r.db.Exec(ctx, query, fullURL, shortURL, userID)
 	return err
 }
 
@@ -35,4 +36,14 @@ func (r *PostgresRepository) GetURL(ctx context.Context, shortURL string) (strin
 	}
 
 	return fullURL, nil
+}
+
+func (r *PostgresRepository) CreateUser(ctx context.Context, userID string) error {
+	query := `INSERT INTO users (id) VALUES ($1) ON CONFLICT (id) DO NOTHING`
+	_, err := r.db.Exec(ctx, query, userID)
+	return err
+}
+
+func (r *PostgresRepository) GetURLsByUserID(ctx context.Context, userID string) ([]string, error) {
+	return nil, nil // TODO: Доделать до 29-го
 }
--- service/shorter-service/internal/repository/url.go
+++ service/shorter-service/internal/repository/url.go
@@ -1,10 +1,13 @@
 package repository
 
-import (
-	"context"
-)
+import "context"
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, userID string) error
+}
 
 type URLRepository interface {
-	SaveURL(ctx context.Context, fullURL, shortURL string) error
+	SaveURL(ctx context.Context, fullURL, shortURL, userID string) error
 	GetURL(ctx context.Context, shortURL string) (string, error)
+	GetURLsByUserID(ctx context.Context, userID string) ([]string, error)
 }
