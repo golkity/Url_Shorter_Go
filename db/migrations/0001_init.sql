8ebe62e7360099f66193c707a2dce73bd2b2429e
Finnik <73883383+golkity@users.noreply.github.com>
2025-09-25 09:59:57 +0300

    feat(db): add new table for auth-service(users) and update table for url-shortner

--- db/migrations/0001_init.sql
+++ db/migrations/0001_init.sql
@@ -1,6 +1,34 @@
-CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
-    full_url TEXT NOT NULL,
-    short_url VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+
+    id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email         TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
 );
+
+CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
+
+CREATE TABLE IF NOT EXISTS users (
+
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
+);
+
+CREATE TABLE IF NOT EXISTS urls (
+
+    id         SERIAL PRIMARY KEY,
+    full_url   TEXT NOT NULL,
+    short_url  VARCHAR(20) UNIQUE NOT NULL,
+    user_id    UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+
+);
+
+CREATE INDEX IF NOT EXISTS idx_urls_short_url ON urls(short_url);
+CREATE INDEX IF NOT EXISTS idx_urls_user_id ON urls(user_id);
