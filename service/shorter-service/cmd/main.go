08a0eb6d882501a6aa669551f42f2719d126a7ea
Finnik <73883383+golkity@users.noreply.github.com>
2025-09-25 11:15:18 +0300

    feat(main): add JWT, KAFKA

--- service/shorter-service/cmd/main.go
+++ service/shorter-service/cmd/main.go
@@ -2,36 +2,52 @@ package main
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 	"url-shortener/internal/custom_errors"
-	"url-shortener/internal/handler"
+	http2 "url-shortener/internal/handler/http"
 	"url-shortener/internal/handler/routes"
+	"url-shortener/internal/kafka"
 	"url-shortener/internal/repository"
 	"url-shortener/internal/service"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-	Port := os.Getenv("APP_PORT")
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	port := os.Getenv("APP_PORT")
 	dbURL := os.Getenv("POSTGRES_DSN")
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
+	kafkaBrokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
 
 	dbpool, err := pgxpool.New(context.Background(), dbURL)
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+		log.Error("Unable to connect to database", "error", err)
+		os.Exit(1)
 	}
 	defer dbpool.Close()
 
 	urlRepo := repository.NewPostgresRepository(dbpool)
+	var userRepo repository.UserRepository = urlRepo
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go kafka.RunConsumer(ctx, log, kafkaBrokers, userRepo)
 
 	urlService := service.NewURLService(urlRepo)
-	urlHandler := handler.NewURLHandler(urlService)
-	router := routes.NewRouter(urlHandler)
+	urlHandler := http2.NewURLHandler(urlService)
 
-	log.Printf("Swwager: http://localhost:%s/swagger/index.html", Port)
+	router := routes.NewRouter(urlHandler, jwtSecret)
 
-	if err := http.ListenAndServe(":"+Port, router); err != nil {
-		log.Fatalf("%s : %v", custom_errors.Error_StartServer, err)
+	log.Info("Swagger UI available", "url", "http://localhost:"+port+"/swagger/index.html")
+	log.Info("Starting server", "port", port)
+
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Error(custom_errors.Error_StartServer.Error(), "error", err)
+		os.Exit(1)
 	}
 }
